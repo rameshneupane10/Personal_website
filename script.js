a803b694654fee59347fdf00baf867574a9336af
rameshneupane10 <rameshnjr10@gmail.com>
2023-11-30 10:43:26 +0545

    day5

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-var tab=document.getElementsByClassName("tab");
+var tabs=document.getElementsByClassName("tab");
 var tabcontents=document.getElementsByClassName("tab-contents");
 
 function Opentab(tabname)
 {
-    for(tablink of tab)
+    for(tab of tabs)
     {
         tab.classList.remove("active-link");
     }
     for(tabcontent of tabcontents)
     {
-        tabcontents.classList.remove("active-tab");
+        tabcontent.classList.remove("active-tab");
     }
+    document.getElementById(tabname).classList.add("active-tab");
 }
 
